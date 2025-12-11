bf0807d6e1d4b854089f0b3cbf96b73b96782f22
NaoyaHayashi37 <naochakku@gmail.com>
2025-12-12 03:21:35 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         {/* ヘッダー開始 */}
         <header className="bg-gray-800 text-white p-4">
           <nav className="flex justify-between max-w-5xl mx-auto">
